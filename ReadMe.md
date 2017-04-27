d207484fafae226bc6710599402b4c540a7546e7
fernandobd42 <fernandolucasbd@gmail.com>
2017-04-27 11:15:11 +0000

    boilerplate

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+## building an application to get weather forecast
+
+### to init, run:
+
+**first:** npm install && webpack
+
+**then:** node server.js
---
+++ app/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import {Route, Router, IndexRoute, hashHistory} from 'react-router'
+
+
+ReactDOM.render(
+    <h2>boilerplate</h2>,
+  document.getElementById('app')
+);
