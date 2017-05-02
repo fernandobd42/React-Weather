2ed9e19cd30f77cce9e16987299a437a5fcb0183
fernandobd42 <fernandolucasbd@gmail.com>
2017-05-02 21:38:40 +0000

    making api call

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
 import WeatherForm from 'WeatherForm'
 import WeatherMessage from 'WeatherMessage'
+import openWeatherMap from 'openWeatherMap'
 
 const Weather = React.createClass({
+  getInitialState: function () {
+    return {
+      isLoading: false
+    }
+  },
+  handleSearch: function (location) {
+    var that = this;
+
+    openWeatherMap.getTemp(location).then( function (temp) {
+      that.setState({
+        location: location,
+        temp: temp
+      });
+    }, function (errorMessage) {
+      alert(errorMessage)
+    });
+  },
   render: function () {
+    var {temp, location} = this.state;
+
     return (
       <div>
         <h3>Weather Component</h3>
-        <WeatherForm/>
-        <WeatherMessage/>
+        <WeatherForm onSearch={this.handleSearch}/>
+        <WeatherMessage temp={temp} location={location}/>
       </div>
     );
   }
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const WeatherMessage = React.createClass({
   render: function () {
+    var {temp, location} = this.props;
+
     return (
       <div>
-        <h3>It's it 12 in Dois Vizinhos</h3>
+        <h3>It's it {temp} in {location}</h3>
       </div>
     );
   }
